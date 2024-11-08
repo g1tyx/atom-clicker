987a9ecf7df514600c976b23705121fc494a9d07
Ayfri <pierre.ayfri@gmail.com>
2024-11-09 00:39:59 +0100

    feat(numbers): Display much higher numbers correctly.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,9 +1,69 @@
+const SUFFIXES = [
+	"",
+	"K",
+	"M",
+	"B",
+	"T",
+	"Qa",
+	"Qi",
+	"Sx",
+	"Sp",
+	"Oc",
+	"No",
+	"Dc",
+	"UDc",
+	"DDc",
+	"TDc",
+	"QaDc",
+	"QiDc",
+	"SxDc",
+	"SpDc",
+	"OcDc",
+	"NoDc",
+	"Vg",
+	"UVg",
+	"DVg",
+	"TVg",
+	"QaVg",
+	"QiVg",
+	"SxVg",
+	"SpVg",
+	"OcVg",
+	"NoVg",
+	"Tg",
+	"UTg",
+	"DTg",
+	"TTg",
+	"QaTg",
+	"QiTg",
+	"SxTg",
+	"SpTg",
+	"OcTg",
+	"NoTg",
+	"Qag",
+	"UQag",
+	"DQag",
+	"TQag"
+];
+
 export function formatNumber(num: number): string {
-	return new Intl.NumberFormat('en', {
-		notation: 'compact',
-		compactDisplay: 'short',
-		maximumFractionDigits: 2
-	}).format(num);
+	if (!Number.isFinite(num)) {
+		throw new Error("Le nombre doit être fini.");
+	}
+
+	const absNum = Math.abs(num);
+	const sign = num < 0 ? "-" : "";
+
+	if (absNum < 1000) {
+		return `${num}`;
+	}
+
+	const exponent = Math.floor(Math.log(absNum) / Math.log(1000));
+	const suffixIndex = Math.min(exponent, SUFFIXES.length - 1);
+	const suffix = SUFFIXES[suffixIndex];
+	const scaled = num / Math.pow(1000, suffixIndex);
+
+	return `${sign}${scaled.toFixed(2)}${suffix}`;
 }
 
 export function shortNumberText(num: number): string {
